8f68f268afa45a36eb56cdf49d7a224772db6a50
Nikhil <nikhil.raj.cg@gmail.com>
2026-04-25 14:58:05 +0530

    store the reducer

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
-import userReducer from "../features/user/userSlice";
-import bookingReducer from "../features/booking/bookingSlice";
-import servicesReducer from "../features/services/servicesSlice";
-import uiReducer from "../features/ui/uiSlice";
+import authReducer from "../features/authSlice";
+import userReducer from "../features/userSlice";
+import bookingReducer from "../features/bookingSlice";
+import servicesReducer from "../features/servicesSlice";
+import uiReducer from "../features/uiSlice";
 
 export const store = configureStore({
   reducer: {
